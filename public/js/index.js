6b13c4715921332c5138c93c400cc081dcc195be
Koty Wong <kotywong@Kotys-MacBook-Pro.local>
2016-06-27 09:50:29 -0400

    setting up react

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+var LyricApp = React.createClass({
+  render: function() {
+  	return(<div className="start">
+  		<h1>Welcome to the best project fam</h1>
+  		</div>)
+  }
+})
+
+
+ReactDOM.render(<LyricApp/>,
+	document.getElementById('container'));
